03e52f496cf676bf2193b197c2dfab2111455cb4
Maflex24 <maflex24@gmail.com>
2022-06-14 01:29:03 +0200

    Questions Post & Delete

---
+++ Entities/Dtos/QuestionPostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowEntities.Entities.Dtos
+{
+    public class QuestionPostModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid AuthorId { get; set; }
+        //public List<Tag>? Tags { get; set; } = new List<Tag>(); // todo implement tag adding
+    }
+}
