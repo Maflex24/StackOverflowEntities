a9b9f9de88f38cba666efb471bc23efd84e5a417
Maflex24 <maflex24@gmail.com>
2022-06-17 10:12:54 +0200

    Tags Endpoints finished

--- Endpoints/TagsEndpoints.cs
+++ Endpoints/TagsEndpoints.cs
@@ -28,8 +28,78 @@ namespace StackOverflowEntities
             }).WithTags("Tags");
 
 
+            app.MapGet("tag", async (StackOverflowContext db, int tagId, int page, int resultsPerPage) =>
+            {
+                var checkedTag = db.Tags
+                    .FirstOrDefault(t => t.Id == tagId);
+
+                var query = db.Questions
+                    .AsNoTracking()
+                    .Where(q => q.Tags.Contains(checkedTag));
+
+                var skippedElements = resultsPerPage * (page - 1);
+                var totalQuestionsQty = query.Count();
+
+                var questions = await query
+                    .AsNoTracking()
+                    .Skip(skippedElements)
+                    .Take(resultsPerPage)
+                    .Select(q => new QuestionInfoDto()
+                    {
+                        Comments = q.Comments.Count,
+                        ContentShort = q.Content.Substring(0, 200),
+                        Id = q.Id,
+                        Rating = q.Rating,
+                        Replies = q.Replies.Count,
+                        Title = q.Title,
+                        Tags = q.Tags
+                    })
+                    .ToListAsync();
+
+                var result = new AllQuestionsPageDto(totalQuestionsQty, page, resultsPerPage, questions);
+
+                return result;
+
+            }).WithTags("Tags");
+
+            app.MapPost("tag", async (StackOverflowContext db, string tagName) =>
+            {
+                var existed = await db.Tags.Select(t => t.Name == tagName).FirstAsync();
+
+                if (!existed)
+                {
+                    var newTag = new Tag() {Name = tagName};
+                    await db.Tags.AddAsync(newTag);
+                    await db.SaveChangesAsync();
+                    return newTag;
+                }
+
+                return null;
+
+            }).WithTags("Tags");
+
+            app.MapPut("tag", async (StackOverflowContext db, int tagId, string newName) =>
+            {
+                var tag = db.Tags.FirstOrDefault(t => t.Id == tagId);
+                tag.Name = newName;
+
+                await db.SaveChangesAsync();
+
+            }).WithTags("Tags");
+
+
+            app.MapDelete("tag", async (StackOverflowContext db, int tagId) =>
+            {
+                var tag = new Tag() {Id = tagId};
+
+                db.Tags.Remove(tag);
+                await db.SaveChangesAsync();
+
+            }).WithTags("Tags");
+
             return app;
         }
+
 
     }
 }
