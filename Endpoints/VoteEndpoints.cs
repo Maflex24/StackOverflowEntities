8d016e8ea3c68143282506dca34b31d5576b3ed2
Maflex24 <maflex24@gmail.com>
2022-06-17 18:42:13 +0200

    Added Vote service

---
+++ Endpoints/VoteEndpoints.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StackOverflowEntities.Entities;
+
+namespace StackOverflowEntities.Endpoints
+{
+    public static class VoteEndpoints
+    {
+        public static WebApplication RegisterVoteEndpoints(this WebApplication app)
+        {
+            app.MapPost("vote", async (StackOverflowContext db, Guid elementId, Guid userId, int voteValue) =>
+            {
+                if (voteValue != -1 && voteValue != 1)
+                    return null;
+
+                var element = await db.QuestionReplyCommentModels.FirstOrDefaultAsync(e => e.Id == elementId);
+                var user = await db.Users.FirstOrDefaultAsync(u => u.Id == userId);
+
+                if (user == null || element == null)
+                    return null;
+
+                var vote = await db.Votes
+                    .FirstOrDefaultAsync(v => v.UserId == userId && v.ElementId == elementId);
+
+                if (vote == null)
+                {
+                    var newVote = new Vote()
+                    {
+                        ElementId = elementId,
+                        UserId = userId,
+                        Value = voteValue
+                    };
+
+                    await db.Votes.AddAsync(newVote);
+                    element.Rating += voteValue;
+                    await db.SaveChangesAsync();
+                    return newVote;
+                }
+
+                var elementsSum = vote.Value + voteValue;
+                if (elementsSum < -1 || elementsSum > 1) return null;
+
+                vote.Value = elementsSum;
+                element.Rating += voteValue;
+
+                if (vote.Value == 0)
+                {
+                    db.Votes.Remove(vote);
+                }
+                await db.SaveChangesAsync();
+                return vote;
+
+
+            }).WithTags("Vote");
+
+            return app;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -35,8 +35,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-
-
 using var scope = app.Services.CreateScope();
 var dbContext = scope.ServiceProvider.GetService<StackOverflowContext>();
 
@@ -54,82 +52,6 @@ app.RegisterTagsEndpoints();
 app.RegisterReplyEndpoints();
 app.RegisterCommentEndpoints();
 app.RegisterUsersEndpoints();
+app.RegisterVoteEndpoints();
 
 app.Run();
-
-
-
-
-
-
-
-//app.MapPost("questions", async (StackOverflowContext db, [FromBodyAttribute] string content, [FromQueryAttribute] Guid authorId) =>
-//{
-//    var question = new Question()
-//    {
-//        AuthorId = authorId,
-//        Content = content,
-//        Created = DateTime.Now
-//    };
-
-//    await db.Questions.AddAsync(question);
-//    await db.SaveChangesAsync();
-//});
-
-//app.MapPost("reply", async (StackOverflowContext db, [FromBodyAttribute] string content, [FromQueryAttribute] Guid questionId, [FromQueryAttribute] Guid authorId) =>
-//{
-//    var reply = new Reply()
-//    {
-//        AuthorId = authorId,
-//        Content = content,
-//        QuestionId = questionId,
-//        Created = DateTime.Now
-//    };
-
-//    await db.Replies.AddAsync(reply);
-//    await db.SaveChangesAsync();
-
-//    return reply;
-//});
-
-//app.MapPost("comment", async (StackOverflowContext db, [FromBodyAttribute] string content, [FromQueryAttribute] Guid elementId, [FromQueryAttribute] Guid authorId) =>
-//{
-//    var comment = new Comment()
-//    {
-//        AuthorId = authorId,
-//        Content = content,
-//        Created = DateTime.Now
-//    };
-
-//    var discriminator = await db.DiscriminatorViews
-//        .FirstAsync(e => e.Id == elementId);
-
-//    var value = discriminator.Discriminator == "Question"
-//        ? comment.QuestionId = elementId
-//        : comment.ReplyId = elementId;
-
-//    await db.AddAsync(comment);
-//    await db.SaveChangesAsync();
-
-//    return comment;
-//});
-
-//app.MapGet("users", async (StackOverflowContext db) =>
-//{
-//    var users = await db.Users
-//        .AsNoTracking()
-//        .Select(u => new { u.Id, u.Name })
-//        .ToListAsync();
-
-//    return users;
-//});
-
-//app.MapPost("users", async (StackOverflowContext db, string userName) =>
-//{
-//    var newUser = new User() { Name = userName };
-
-//    await db.Users.AddAsync(newUser);
-//    await db.SaveChangesAsync();
-
-//    return newUser;
-//});
