8642027c327609f164db691ae101f3a1d8a879e6
Maflex24 <maflex24@gmail.com>
2022-06-17 11:37:29 +0200

    Added Comments endpoints

---
+++ Entities/Dtos/CommentPostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowEntities.Entities.Dtos
+{
+    public class CommentPostModel
+    {
+        public Guid? QuestionId { get; set; }
+        public Guid? ReplyId { get; set; }
+        public Guid AuthorId { get; set; }
+        public string Content { get; set; }
+    }
+}
