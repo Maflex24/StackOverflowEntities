5e3c91b2a8bec5965f5a3406a9e411a6b8b9e71e
Maflex24 <maflex24@gmail.com>
2022-06-17 13:24:22 +0200

    Get Users endpoint

---
+++ Entities/Dtos/UsersDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowEntities.Entities.Dtos
+{
+    public class UsersDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Questions { get; set; }
+        public int Replies { get; set; }
+        public int Comments { get; set; }
+    }
+}
